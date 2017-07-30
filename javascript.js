fbb769bb40b18d5e845385c42df3d3719e050113
Yatendra <rubyyataw2100@gmail.com>
2017-07-30 08:41:52 -0700

    Added all first week of work to ClassWork on Github

---
+++ javascript.js
@@ -0,0 +1,19 @@
+ <script type="text/javascript">
+    function myColor(){
+   document.getElementById('box').style.background-color:blue;
+    }
+    function myFunction(){
+    	document.getElementById('box').style.height = 190px;
+    }
+
+   function fade(){
+   	document.getElementById('box').style.
+   }
+   function reset(){
+   	document.getEmentByid('box').style.height:150px; width:150px; background-color:orange; margin:25px;
+   }
+  </script>
+    
+   
+
+ 
